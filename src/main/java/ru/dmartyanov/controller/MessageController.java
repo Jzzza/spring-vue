68db2bdeb23ef7dd2e41852537b9712aeac6be27
Jzzza <jzzza1@gmail.com>
2020-11-19 03:59:54 +0300

    Added base CRUD operations for message

--- src/main/java/ru/dmartyanov/controller/MessageController.java
+++ src/main/java/ru/dmartyanov/controller/MessageController.java
@@ -1,9 +1,6 @@
 package ru.dmartyanov.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.dmartyanov.exceptions.NotFoundException;
 
 import java.util.ArrayList;
@@ -14,7 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+    private int counter = 4;
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String, String>() {{
             put("id", "1");
             put("text", "First message");
@@ -38,6 +36,39 @@ public class MessageController {
     public Map<String, String> getOne(
             @PathVariable String id
     ) {
+        return getMessage(id);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(
+            @PathVariable String id,
+            @RequestBody Map<String, String> message
+    ) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(
+            @PathVariable String id
+    ){
+        Map<String, String> message = getMessage(id);
+
+        messages.remove(message);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter(x->x.get("id").equals(id))
                 .findFirst()
