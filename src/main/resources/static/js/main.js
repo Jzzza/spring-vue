4682aea69773f0b2cf44981c04b4275df3156b14
Jzzza <jzzza1@gmail.com>
2020-11-20 23:46:10 +0300

    Added simple http request

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,5 @@
+var messageApi = Vue.resource('/message{/id}');
+
 Vue.component('message-row',{
   props: ['message'],
   template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
@@ -5,7 +7,16 @@ Vue.component('message-row',{
 
 Vue.component('messages-list',{
   props: ['messages'],
-  template: '<div><message-row v-for="message in messages" :message="message" /></div>'
+  template: '<div>' +
+                '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+            '</div>',
+  created: function() {
+    messageApi.get().then(result=>
+      result.json().then(data=>
+        data.forEach(message => this.messages.push(message))
+      )
+    )
+  }
 });
 
 var app = new Vue({
@@ -13,9 +24,6 @@ var app = new Vue({
   template: '<messages-list :messages="messages" />',
   data: {
     messages: [
-      {id: '1', text: 'First'},
-      {id: '2', text: 'Second'},
-      {id: '3', text: 'Third'}
     ]
   }
 });
