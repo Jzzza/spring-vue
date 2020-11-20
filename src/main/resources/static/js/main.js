1bb2a12eb920cdef7048c0d775bed8720bc71dbd
Jzzza <jzzza1@gmail.com>
2020-11-20 23:32:55 +0300

    Added output multiple messages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,16 @@
+Vue.component('messages-list',{
+  props: ['messages'],
+  template: '<div><div v-for="message in messages">{{ message.text }}</div></div>'
+});
+
 var app = new Vue({
   el: '#app',
+  template: '<messages-list :messages="messages" />',
   data: {
-    message: 'Привет, Vue!'
+    messages: [
+      {id: '1', text: 'First'},
+      {id: '2', text: 'Second'},
+      {id: '3', text: 'Third'}
+    ]
   }
 });
