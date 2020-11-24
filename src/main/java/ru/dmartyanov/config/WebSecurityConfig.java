9f60dbd7de65698b8c9ff6a9dce44dd0bd259df2
Jzzza <jzzza1@gmail.com>
2020-11-24 09:28:34 +0300

    Updated configuration

--- src/main/java/ru/dmartyanov/config/WebSecurityConfig.java
+++ src/main/java/ru/dmartyanov/config/WebSecurityConfig.java
@@ -1,10 +1,18 @@
 package ru.dmartyanov.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import ru.dmartyanov.domain.User;
+import ru.dmartyanov.repo.UserDetailsRepo;
 
 @Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,5 +21,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Bean
+    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
+        return map -> {
+            return new User();
+        };
     }
 }
