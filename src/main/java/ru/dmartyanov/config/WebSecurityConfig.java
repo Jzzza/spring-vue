6edd6a00f9ef449155809556bc8f0c42bb733809
Jzzza <jzzza1@gmail.com>
2020-11-25 06:34:38 +0300

    Update WebSecurityConfig for access inner resources

--- src/main/java/ru/dmartyanov/config/WebSecurityConfig.java
+++ src/main/java/ru/dmartyanov/config/WebSecurityConfig.java
@@ -18,8 +18,9 @@ import java.time.LocalDateTime;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/").permitAll()
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/","/login**","/webjars/**","/error**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
