594224c9727439fc0f6044be5acf5faf479d23c8
Jzzza <jzzza1@gmail.com>
2020-11-25 07:02:49 +0300

    Added filling messages

--- src/main/java/ru/dmartyanov/controller/MainController.java
+++ src/main/java/ru/dmartyanov/controller/MainController.java
@@ -1,17 +1,26 @@
 package ru.dmartyanov.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.dmartyanov.domain.User;
+import ru.dmartyanov.repo.MessageRepo;
 
 import java.util.HashMap;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping
     public String main(
             Model model,
@@ -19,7 +28,7 @@ public class MainController {
         HashMap<Object, Object> data = new HashMap<>();
 
         data.put("profile", user);
-        data.put("messages", null);
+        data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
         return "index";
