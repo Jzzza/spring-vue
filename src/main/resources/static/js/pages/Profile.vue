67e36e4bb36f3f66fecfb390465316c1c09bdf1c
Jzzza <jzzza1@gmail.com>
2020-12-01 21:45:28 +0300

    Add profile page partially

---
+++ src/main/resources/static/js/pages/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout justify-space-around>
+      <v-flex>
+        <div>User profile</div>
+        <v-layout row justify-space-between>
+          <v-flex>
+            <v-img :src="profile.userpic"></v-img>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+  export default{
+    name: 'Profile',
+    computed: mapState(['profile'])
+  }
+</script>
+
+<style>
+</style>
