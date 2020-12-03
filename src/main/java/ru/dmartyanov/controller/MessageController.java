73cd6e5a83976ff1018b034450c5a067a174ce46
Jzzza <jzzza1@gmail.com>
2020-12-03 10:06:00 +0300

    Update backend for work with metadata

--- src/main/java/ru/dmartyanov/controller/MessageController.java
+++ src/main/java/ru/dmartyanov/controller/MessageController.java
@@ -1,22 +1,36 @@
 package ru.dmartyanov.controller;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.dmartyanov.domain.Message;
+import ru.dmartyanov.domain.MetaDto;
 import ru.dmartyanov.domain.Views;
 import ru.dmartyanov.dto.EventType;
 import ru.dmartyanov.dto.ObjectType;
 import ru.dmartyanov.repo.MessageRepo;
 import ru.dmartyanov.util.WsSender;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    private static String URL_PATTERN = "https?:\\/\\/?[\\w\\d\\._\\-%\\/\\?=&#]+";
+    private static String IMAGE_PATTERN = "\\.(jpeg|jpg|git|png)$";
+
+    private static Pattern URL_REGEX = Pattern.compile(URL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static Pattern IMG_REGEX = Pattern.compile(IMAGE_PATTERN, Pattern.CASE_INSENSITIVE);
+
     private final MessageRepo messageRepo;
     private final BiConsumer<EventType, Message> wsSender;
 
@@ -39,8 +53,9 @@ public class MessageController {
     }
 
     @PostMapping
-    public Message create(@RequestBody Message message) {
+    public Message create(@RequestBody Message message) throws IOException {
         message.setCreationDate(LocalDateTime.now());
+        fillMeta(message);
         Message createdMessage = messageRepo.save(message);
         wsSender.accept(EventType.CREATE, createdMessage);
         return createdMessage;
@@ -50,8 +65,9 @@ public class MessageController {
     public Message update(
             @PathVariable("id") Message messageFromDb,
             @RequestBody Message message
-    ) {
+    ) throws IOException {
         BeanUtils.copyProperties(message, messageFromDb, "id");
+        fillMeta(messageFromDb);
         Message updatedMessage = messageRepo.save(messageFromDb);
         wsSender.accept(EventType.UPDATE, updatedMessage);
         return updatedMessage;
@@ -63,5 +79,44 @@ public class MessageController {
     ) {
         messageRepo.delete(message);
         wsSender.accept(EventType.REMOVE, message);
+    }
+
+    private void fillMeta(Message message) throws IOException {
+        String text = message.getText();
+        Matcher matcher = URL_REGEX.matcher(text);
+        if (matcher.find()) {
+            String url = text.substring(matcher.start(), matcher.end());
+
+            matcher = IMG_REGEX.matcher(url);
+
+            message.setLink(url);
+
+            if (matcher.find()) {
+                message.setLinkCover(url);
+            } else if (!url.contains("youtu")) {
+                MetaDto meta = getMeta(url);
+
+                message.setLinkCover(meta.getCover());
+                message.setLinkTitle(meta.getTitle());
+                message.setLinkDescription(meta.getDescription());
+            }
+        }
+    }
+
+    private MetaDto getMeta(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements title = doc.select("meta[name$=title],meta[property$=title]");
+        Elements description = doc.select("meta[name$=description],meta[property$=description]");
+        Elements cover = doc.select("meta[name$=image],meta[property$=image]");
+
+        return new MetaDto(
+                getContent(title.first()),
+                getContent(description.first()),
+                getContent(cover.first())
+        );
+    }
+
+    private String getContent(Element element) {
+        return element == null ? "" : element.attr("content");
     }
 }
--- src/main/java/ru/dmartyanov/domain/MetaDto.java
+++ src/main/java/ru/dmartyanov/domain/MetaDto.java
@@ -1,8 +1,10 @@
 package ru.dmartyanov.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class MetaDto {
     private String title;
     private String description;
