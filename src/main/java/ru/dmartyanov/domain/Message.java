64194ba0ac2feff6692ccad4641d9143390eae6d
Jzzza <jzzza1@gmail.com>
2020-12-03 09:41:04 +0300

    Add lambok annotation

--- src/main/java/ru/dmartyanov/domain/Message.java
+++ src/main/java/ru/dmartyanov/domain/Message.java
@@ -2,6 +2,7 @@ package ru.dmartyanov.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Table
 @ToString(of = {"id","text"})
 @EqualsAndHashCode(of = {"id"})
+@Data
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,28 +27,4 @@ public class Message {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonView(Views.FullMessage.class)
     private LocalDateTime creationDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
 }
