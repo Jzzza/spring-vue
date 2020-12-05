3d8954d199d350b76d4ec1b76f0f0ac4e6d212ee
Jzzza <jzzza1@gmail.com>
2020-12-05 10:51:52 +0300

    Fix naming

--- src/main/java/ru/dmartyanov/repo/CommentRepo.java
+++ src/main/java/ru/dmartyanov/repo/CommentRepo.java
@@ -3,11 +3,10 @@ package ru.dmartyanov.repo;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.dmartyanov.domain.Comment;
-import ru.dmartyanov.domain.Message;
 
 import java.util.List;
 
 public interface CommentRepo extends JpaRepository<Comment, Long> {
     @EntityGraph(attributePaths = {"comments"})
-    List<Message> findaAll();
+    List<Comment> findAll();
 }
