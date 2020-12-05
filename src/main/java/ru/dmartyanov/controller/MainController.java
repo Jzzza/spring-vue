56a6c70e4f6c58c5bd0df3fcf77c249f2d475a8c
Jzzza <jzzza1@gmail.com>
2020-12-05 10:56:50 +0300

    Partially update MainController

--- src/main/java/ru/dmartyanov/controller/MainController.java
+++ src/main/java/ru/dmartyanov/controller/MainController.java
@@ -1,5 +1,7 @@
 package ru.dmartyanov.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.dmartyanov.domain.User;
+import ru.dmartyanov.domain.Views;
 import ru.dmartyanov.repo.MessageRepo;
 
 import java.util.HashMap;
@@ -16,13 +19,14 @@ import java.util.HashMap;
 @RequestMapping("/")
 public class MainController {
     private final MessageRepo messageRepo;
-
+    private final ObjectWriter writer;
     @Value("${spring.profiles.active}")
     private String profile;
 
     @Autowired
-    public MainController(MessageRepo messageRepo) {
+    public MainController(MessageRepo messageRepo, ObjectMapper mapper) {
         this.messageRepo = messageRepo;
+        this.writer = mapper.setConfig(mapper.getSerializationConfig()).writerWithView(Views.FullMessage.class);
     }
 
     @GetMapping
