47b79b4bdf6ca2ccb54b399ea39b1dbf3a9eeee1
Jzzza <jzzza1@gmail.com>
2020-12-05 10:43:57 +0300

    Added entity graph for repo

--- src/main/java/ru/dmartyanov/repo/CommentRepo.java
+++ src/main/java/ru/dmartyanov/repo/CommentRepo.java
@@ -1,7 +1,13 @@
 package ru.dmartyanov.repo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.dmartyanov.domain.Comment;
+import ru.dmartyanov.domain.Message;
+
+import java.util.List;
 
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = {"comments"})
+    List<Message> findaAll();
 }
