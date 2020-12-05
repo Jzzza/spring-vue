1db1ca9c51d1923daa9e1d169db3324ae9b16de4
Jzzza <jzzza1@gmail.com>
2020-12-05 10:50:59 +0300

    Added CommentController

---
+++ src/main/java/ru/dmartyanov/controller/CommentController.java
@@ -0,0 +1,30 @@
+package ru.dmartyanov.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dmartyanov.domain.Comment;
+import ru.dmartyanov.domain.User;
+import ru.dmartyanov.service.CommentService;
+
+@RestController
+@RequestMapping("comment")
+public class CommentController {
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public Comment create(
+            @RequestBody Comment comment,
+            @AuthenticationPrincipal User user
+    ) {
+        return commentService.create(comment, user);
+    }
+}
